6a5215df90928beccea10b7e551b70ff7ec50591
Vedant Gupta <88703589+VedantGames@users.noreply.github.com>
2024-09-24 00:16:59 +0530

    Update index.js

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   console.log('Connecting to MongoDB...');
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGO_URL);
+    const connectionInstance = await mongoose.connect('mongodb+srv://guptavedant2549:vedant2549@cluster0.kqw5bfo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB connected successfully !! DB Host ' + connectionInstance.connection.host)
   } catch (err) {
     console.log('MongoDB connection failed ' + err);
